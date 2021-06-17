991a99a78479c4dff524c35798f3c26c2d9e1c63
Chrys <chrys@linux-a11y.org>
2021-06-17 17:27:29 +0200

    add dynamic API Manager

---
+++ src/orca/dynamic_api_manager.py
@@ -0,0 +1,23 @@
+import gi
+from gi.repository import GObject
+
+
+class DynamicApiManager():
+    def __init__(self, app):
+        self.app = app
+        self.orcaAPI = {'Orca': self.app}
+    def registerAPI(self, key, value):
+        # add dynamic API
+        self.orcaAPI[key] = value
+    def unregisterAPI(self, key):
+        try:
+            del self.orcaAPI[key]
+        except:
+            print('API Key: "{}" not found,'.format(key))
+    def getAPI(self, key):
+        # get dynamic API
+        return self.orcaAPI.get(key)
+    def registerSignal(self, signalName, signalFlag = GObject.SignalFlags.RUN_LAST, closure = GObject.TYPE_NONE, accumulator=()):
+        # register signal
+        if not self.signalExist(signalName):
+            GObject.signal_new(signalName, self, signalFlag, closure,accumulator)
