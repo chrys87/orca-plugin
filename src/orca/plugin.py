56173175ea689891605c9feb18728bfc99a0b56f
Chrys <chrys@linux-a11y.org>
2021-07-05 13:11:52 +0200

    add args, kwargs to plugin init

--- src/orca/plugin.py
+++ src/orca/plugin.py
@@ -1,7 +1,7 @@
 class Plugin():
     #__gtype_name__ = 'BasePlugin'
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.API = None
     def setAPI(self, API):
         self.API = API
